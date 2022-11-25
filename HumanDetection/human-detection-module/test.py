72affb5aab450d7c03a332ef2c2bf2ac4eb51843
Vicente <vicente.costa@ua.pt>
2022-11-25 17:20:11 +0000

    differnt test

--- HumanDetection/human-detection-module/test.py
+++ HumanDetection/human-detection-module/test.py
@@ -1,6 +1,6 @@
 import requests
 
-obj = {"cameraId":1, "timestamp":"2022-11-21 11:50:24.537430"}
+obj = {"cameraId":1, "timestamp":"2022-11-21 20:46:15.111111"}
 intrusionAPI_url = "http://localhost:8083/intrusion"
 x = requests.post(intrusionAPI_url, json = obj)
 print("nice")
