144c25ea7850cc6062c73feb0e6c0b2bf093f2ab
Vicente <vicente.costa@ua.pt>
2022-12-16 14:30:12 +0000

    uncomment microservice connection

--- HumanDetection/camera/camera.py
+++ HumanDetection/camera/camera.py
@@ -62,8 +62,8 @@ class Camera:
         }
         url =service_registry_url+ "/registry/register"
         print(url)
-        #self.camera_id = requests.post(url, json=data, headers={"Authorization" : "Bearer "+str(self.access_token)}).json()["serviceUniqueId"]
-        self.camera_id = "111cc11-165a-445a-b062-9b7a16195dd6"
+        self.camera_id = requests.post(url, json=data, headers={"Authorization" : "Bearer "+str(self.access_token)}).json()["serviceUniqueId"]
+        #self.camera_id = "36e25c8c-165a-445a-b062-9b7a16195dd6"
         print(self.camera_id)
         #print(self.camera_id.text)
 
@@ -108,15 +108,15 @@ class Camera:
     def get_property_id(self):
         while self.propertyId == None:
             print("getting property id")
-            self.propertyId=10
-            # smapi_response = requests.get(self.smapi_url + "/cameras/" + str(self.camera_id), headers={"Authorization" : str(self.access_token)})
-            # if (smapi_response.status_code == 200):
-            #     smapi_response = smapi_response.json()
-            #     self.propertyId = smapi_response["property"] #tenho que ver o que devolve
-            #     print(self.propertyId)
-            # else:
-            #     print("Request Error. HTTP Error code: " + str(smapi_response.status_code))
-            #     time.sleep(10)
+            #self.propertyId=10
+            smapi_response = requests.get(self.smapi_url + "/cameras/" + str(self.camera_id), headers={"Authorization" : str(self.access_token)})
+            if (smapi_response.status_code == 200):
+                smapi_response = smapi_response.json()
+                self.propertyId = smapi_response["property"] #tenho que ver o que devolve
+                print(self.propertyId)
+            else:
+                print("Request Error. HTTP Error code: " + str(smapi_response.status_code))
+                time.sleep(10)
 
 
     async def transmit_video(self, video_path):
@@ -224,10 +224,10 @@ class Camera:
             print(url)
             #print(files["document"].peek())
             try:
-                #response = requests.post(url, files=files, params=params, headers={"Authorization" : str(self.access_token)})
-                print("no sending video")
-                #print(response.text)
-                #print("Request status: %s" % response.status_code)
+                response = requests.post(url, files=files, params=params, headers={"Authorization" : str(self.access_token)})
+                #print("no sending video")
+                print(response.text)
+                print("Request status: %s" % response.status_code)
             finally:
                 os.remove("temp.mp4")
                 print("Removing temp.mp4")
